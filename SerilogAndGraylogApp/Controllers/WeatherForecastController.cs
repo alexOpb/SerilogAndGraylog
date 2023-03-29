0e5e652f658aca133d86a93936f735a5766ff9cc
Palienko.an <https://adm-itweb-bitbucket.dns-shop.ru/>
2023-03-29 22:12:01 +0600

    Add logging to Get method

--- SerilogAndGraylogApp/Controllers/WeatherForecastController.cs
+++ SerilogAndGraylogApp/Controllers/WeatherForecastController.cs
@@ -21,12 +21,16 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        
+        _logger.LogInformation("{@Forecasts}", new { Array = forecasts });
+
+        return forecasts;
     }
 }
